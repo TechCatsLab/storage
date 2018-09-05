11760197d828b42dc52281f497de1c530191aa1c
zh1014 <964701944@qq.com>
2018-09-05 10:15:43 +0800

    fix

--- check/check.go
+++ check/check.go
@@ -2,68 +2,51 @@ package check
 
 import (
 	"database/sql"
-	"fmt"
-	"strings"
 
 	"github.com/go-gorp/gorp"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const dsnFormat = "root:123456@tcp(127.0.0.1:3306)/%s"
+const dsn = "root:123456@tcp(127.0.0.1:3306)/"
 
 func IsDbExist(dbName string) bool {
-	db := getDb(dbName)
-	return isDbExist(db)
-}
-
-func isDbExist(db *sql.DB) bool {
-	err := db.Ping()
-	if err != nil {
-		if strings.Contains(err.Error(), "Unknown database") {
-			return false
-		}
-		panic(err)
-	}
-	return true
+	db, err := sql.Open("mysql", dsn)
+	checkErr(err)
+	dbmp := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{Engine: "InnoDB", Encoding: "UTF8"}}
+	var count int64
+	count, err = dbmp.SelectInt("SELECT COUNT(0) "+
+		"FROM information_schema.SCHEMATA "+
+		"WHERE SCHEMA_NAME = ?;", dbName)
+	checkErr(err)
+	return count > 0
 }
 
 func IsTableExist(dbName, tableName string) bool {
-	db := getDb(dbName)
-	if !isDbExist(db) {
-		return false
-	}
+	db, err := sql.Open("mysql", dsn)
+	checkErr(err)
 	dbmp := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{Engine: "InnoDB", Encoding: "UTF8"}}
-	count, err := dbmp.SelectInt("SELECT COUNT(0) "+
+	var count int64
+	count, err = dbmp.SelectInt("SELECT COUNT(0) "+
 		"FROM information_schema.TABLES "+
-		"WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = ?;", tableName)
-	if err != nil {
-		panic(err)
-	} else {
-		return count > 0
-	}
+		"WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?;", dbName, tableName)
+	checkErr(err)
+	return count > 0
 }
 
 func IsColExist(dbName, tableName, ColName string) bool {
-	db := getDb(dbName)
-	if !isDbExist(db) {
-		return false
-	}
+	db, err := sql.Open("mysql", dsn)
+	checkErr(err)
 	dbmp := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{Engine: "InnoDB", Encoding: "UTF8"}}
-	count, err := dbmp.SelectInt("SELECT COUNT(0) "+
+	var count int64
+	count, err = dbmp.SelectInt("SELECT COUNT(0) "+
 		"FROM information_schema.COLUMNS "+
-		"WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = ? AND COLUMN_NAME = ?;", tableName, ColName)
-	if err != nil {
-		panic(err)
-	} else {
-		return count > 0
-	}
+		"WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? AND COLUMN_NAME = ?;", dbName, tableName, ColName)
+	checkErr(err)
+	return count > 0
 }
 
-func getDb(dbName string) *sql.DB {
-	dsn := fmt.Sprintf(dsnFormat, dbName)
-	db, err := sql.Open("mysql", dsn)
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-	return db
 }
